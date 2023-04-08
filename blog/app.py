e7f3f888b2dd795616b50a1881f0792af83e19e9
Nikita <kyle.winner@gmail.com>
2023-04-08 19:59:12 +0300

    first views and templates

--- blog/app.py
+++ blog/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from blog.views.users import users_app
+from blog.views.articles import articles_app
 
 app = Flask(__name__)
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
 
-@app.route('/great/<name>/')
-def index(name):
-    return f'Start page. Hello, {name}'
+
+@app.route('/home')
+def index():
+    return render_template('index.html')
+
